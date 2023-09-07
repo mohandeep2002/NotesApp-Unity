b0cab94f3a8150ff5c10d280659154c3027480a5
Mohandeep Atmuri <mohandeep2002@gmail.com>
2023-09-07 22:16:14 +0530

    WIP -> Title and something

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     #region Variables
-    public RectTransform placeHolderHomePage;
-    public RectTransform infoPanel;
+    [Header("Panels")]
+    public GameObject placeHolderHomePage;
+    public GameObject infoPanel;
+    public GameObject mainPanel;
+    public GameObject searchPanel;
+
+    [Header("Texts")]
+    public TMPro.TMP_InputField searchingText;
+
+    [Header("Buttons")]
+    public Button closeSearchButton;
     #endregion
 
+    #region MonoBehaviour Functions
+    private void FixedUpdate()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape) && searchPanel.gameObject.activeInHierarchy)
+            {
+                ToggleSearchPanel(false);
+                ToggleMainPanel(true);
+            }
+        }
+
+        if (searchingText.text.Length == 0) ToggleSearchCloseButton(false);
+        else ToggleSearchCloseButton(true);
+    }
+    #endregion
 
     #region ButtonClicks
     public void OnInfoButtonClicked()
@@ -24,9 +50,20 @@ public class UIManager : MonoBehaviour
             TogglePlaceHolders(false);
         }
     }
+
+    public void OnSearchButtonClicked()
+    {
+        ToggleMainPanel(false);
+        ToggleSearchPanel(true);
+    }
     #endregion
 
     #region UIEvents
+    public void ToggleSearchCloseButton(bool state)
+    {
+        closeSearchButton.gameObject.SetActive(state);
+    }
+
     public void ToggleInfoPanel(bool state)
     {
         infoPanel.gameObject.SetActive(state);
@@ -35,6 +72,16 @@ public class UIManager : MonoBehaviour
     public void TogglePlaceHolders(bool state)
     {
         placeHolderHomePage.gameObject.SetActive(state);
+    }
+
+    public void ToggleSearchPanel(bool state)
+    {
+        searchPanel.gameObject.SetActive(state);
+    }
+
+    public void ToggleMainPanel(bool state)
+    {
+        mainPanel.gameObject.SetActive(state);
     }
     #endregion
 }
